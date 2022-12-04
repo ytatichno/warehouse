8b97b7b7ba8f66ba34ad6a0c8e630838f5ecad83
ytatichno <ytatichno@gmail.com>
2022-12-04 19:58:29 +0300

    frontend inited

---
+++ src/main/warehouse-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button>butt</button>
+      <button>on</button>
+    </div>
+  );
+}
+
+export default App;
