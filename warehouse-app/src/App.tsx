f070c615ca868d2b91a3205b5f0ade59a1ea459b
ytatichno <ytatichno@gmail.com>
2022-12-25 15:48:02 +0300

    Add animations

--- warehouse-app/src/App.tsx
+++ warehouse-app/src/App.tsx
@@ -1,18 +1,93 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 import { getModeForUsageLocation } from 'typescript';
 import { AppControllerService, Good, Rack, Usercard } from './services/openapi';
 import { List } from './components/UI/List/List';
-
+import { Alert, Button, Container } from 'react-bootstrap';
+import { ThemeSwitch } from './components/UI/ThemeSwitch/ThemeSwitch';
+import { Panel } from './components/UI/Panel/Panel';
+import { useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { GetPanel } from './components/UI/Panel/GetPanel';
+import { SendPanel } from './components/UI/Panel/SendPanel';
+import { CSSTransition } from 'react-transition-group';
+import { createRef } from 'react';
+import styles from './components/UI/Panel/Panel.module.css'
+import { createStore } from '@reduxjs/toolkit';
+import { Provider, TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import { useAppDispatch, useAppSelector } from './app/hooks';
 
 function App() {
+  const [theme, setTheme] = useState('dark');
+  const [usercard, setUsercard] = useState<Usercard>();
+  const [view,changeView] = useState('main');
+
+
+  const changeUser = (usercard:Usercard)=>{
+    setUsercard(usercard);
+  }
+  const closeBtn = () =>{
+    changeView('main');
+  }
+  console.log('a');
+  useEffect(()=>{
+    AppControllerService.profile1().then(changeUser)  
+  },[])
+
   return (
-    <div className="App">
+    <div className={theme=='dark'?'DarkApp':'LightApp'}>
+
       <header className="App-header">
-        <div>
-          <List<Good> title="Goods" content={AppControllerService.goods()} />
+        <span >
+          <h2 className={theme=='dark'?'DarkText':'LightText'}>{
+          "Добро пожаловать" +((!!usercard && "firstname" in usercard)?(", " + usercard.firstname):(""))
+          }</h2>
+        </span>
+      
+        <span>
+          <Button variant="w-100 btn btn-lg btn-primary m-4" onClick={()=>{
+            changeView('get')
+          }}>
+            <h3 className={theme=='dark'?'DarkText':'LightText'}>Получить</h3>
+          </Button>
+          <Button variant="w-100 btn btn-lg btn-info m-4" onClick={()=>{
+            changeView('send')
+          }}>
+            <h3 className={theme=='dark'?'DarkText':'LightText'}>Положить</h3>
+          </Button>
+          <ThemeSwitch setTheme={setTheme}/>
+        </span>
+        <Container>
+          <CSSTransition
+          in={view=='get'}
+
+          timeout={1000}
+          classNames="panel"
+          unmountOnExit
+          // onEnter={() => console.log('ent')}
+          // onExited={() => changeView('')}
+        >
+          {/* <GetPanel theme={theme}/> */}
+          {<GetPanel theme={theme} close={closeBtn}/>}
+          </CSSTransition>
+          <CSSTransition
+          in={view=='send'}
+
+          timeout={1000}
+          classNames="panel"
+          unmountOnExit
+          // onEnter={() => console.log('ent')}
+          // onExited={() => changeView('')}
+        >
+          {/* <GetPanel theme={theme}/> */}
+          {<SendPanel theme={theme} close={closeBtn}/>}
+          </CSSTransition>
+          </Container>
+
+        {/* <div>
+          <List<Good> title="Goods" content={c.goods()} />
           <List<Usercard> title="Profiles" content={AppControllerService.profiles()} />
           <List<Rack> title="Racks" content={AppControllerService.racks()}/>
         </div>
@@ -58,10 +133,14 @@ function App() {
           >
             React Redux
           </a>
-        </span>
+        </span> */}
       </header>
     </div>
   );
 }
 
 export default App;
+
+export function isNumeric(value:string|number) {
+  return /^-?\d+$/.test(value.toString());
+}
